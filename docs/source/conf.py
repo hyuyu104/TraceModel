be6100088a8f4abd7f4ac168c3a81e4353eee177
Hongyu <hongyuyu@unc.edu>
2024-09-09 00:55:23 -0400

    Change conf.py and requirements.txt to include markdown files.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ version = '0.0.0'
 source_suffix = {".rst": "restructuredtext", ".md": "markdown"}
 
 extensions = [
+    'myst_parser',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
